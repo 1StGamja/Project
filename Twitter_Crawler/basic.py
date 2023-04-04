56a342940aa5d800804976c7b2728b8484dcae24
Heewon Yu <narsis.yu@gmail.com>
2023-04-04 22:44:17 +0900

    DB연걸정보 별도 분리

--- Twitter_Crawler/basic.py
+++ Twitter_Crawler/basic.py
@@ -1,13 +1,18 @@
-import psycopg2
 import tweepy
-from psycopg2 import sql
+import psycopg2
+import configparser
+from datetime import datetime
 
-# DB 접속 정보
-host = "localhost"
-port = 5432
-database = "postgres"
-user = "postgres"
-password = "dlQmsdl00"
+# db.conn 파일에서 DB 접속 정보 읽기
+config = configparser.ConfigParser()
+config.read('db.conn')
+
+# DB 접속 정보 가져오기
+host = config['DATABASE']['host']
+port = config['DATABASE']['port']
+database = config['DATABASE']['database']
+user = config['DATABASE']['user']
+password = config['DATABASE']['password']
 
 # Twitter API 인증 정보
 consumer_key = "d6Olnocpg0mmlcDz2Rg8R7v2H"
@@ -15,47 +20,44 @@ consumer_secret = "FHZHPwRXyivlfSIwbuvzl1fG0N7HWzciWi1mL8HBxxsbwDeUEN"
 access_token = "1000388956737888256-eWjWZcxpXBytTwd0j9to9XD7SC2xOG"
 access_token_secret = "ncPcuI0zdl4d8zJSyNZFp6oUPviP4s7miig584SgHfPzf"
 
-# OAuth 인증
-auth = tweepy.OAuth1UserHandler(
-    consumer_key, consumer_secret, access_token, access_token_secret
-)
+# 키워드 설정
+keywords = ["보안", "해킹", "유출", "개인정보"]
 
-# API 객체 생성
+# Twitter API 인증
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # DB 연결
 conn = psycopg2.connect(
-    host=host, port=port, database=database, user=user, password=password
+    host=host,
+    port=port,
+    dbname=database,
+    user=user,
+    password=password,
 )
+
+# 커서 생성
 cur = conn.cursor()
 
-# 크롤링할 키워드
-keywords = ["보안", "해킹", "유출", "개인정보", "해킹"]
-
-# 최신 트윗 100건 크롤링 및 DB 저장
+# 키워드별로 최신 트윗 100개 크롤링
 for keyword in keywords:
     print(f"Keyword: {keyword}")
-    tweets = api.search_tweets(q=keyword, count=100, tweet_mode="extended")
+    tweets = api.search_tweets(q=keyword, count=100)
+
     for tweet in tweets:
-        author_name = tweet.user.name
-        author_screen_name = tweet.user.screen_name
-        created_at = tweet.created_at
+        # 트윗 정보 추출
+        username = tweet.user.name
+        screen_name = tweet.user.screen_name
+        created_at = tweet.created_at.strftime("%Y-%m-%d %H:%M:%S")
         full_text = tweet.full_text
 
-        # SQL 문 작성
-        insert_query = sql.SQL(
-            "INSERT INTO {} ({}, {}, {}, {}) VALUES (%s, %s, %s, %s)"
-        ).format(
-            sql.Identifier("tweets"),
-            sql.Identifier("author_name"),
-            sql.Identifier("author_screen_name"),
-            sql.Identifier("created_at"),
-            sql.Identifier("full_text"),
-        )
+        # DB에 저장
+        sql = f"INSERT INTO tweets (username, screen_name, created_at, full_text) VALUES ('{username}', '{screen_name}', '{created_at}', '{full_text}')"
+        cur.execute(sql)
 
-        # 크롤링 결과 DB 저장
-        cur.execute(
-            insert_query,
-            (author_name, author_screen_name, created_at, full_text),
-        )
-        conn.commit()
+# DB에 저장
+conn.commit()
+
+# DB 연결 종료
+conn.close()
