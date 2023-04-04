b43e26a558883abd3760f479faa91245ed456420
Heewon Yu <narsis.yu@gmail.com>
2023-04-04 22:38:54 +0900

    PostgreSQL DB 에 가장 최근 100건만 저장하도록 기능 구현

--- Twitter_Crawler/basic.py
+++ Twitter_Crawler/basic.py
@@ -1,5 +1,13 @@
-
+import psycopg2
 import tweepy
+from psycopg2 import sql
+
+# DB 접속 정보
+host = "localhost"
+port = 5432
+database = "postgres"
+user = "postgres"
+password = "dlQmsdl00"
 
 # Twitter API 인증 정보
 consumer_key = "d6Olnocpg0mmlcDz2Rg8R7v2H"
@@ -15,15 +23,39 @@ auth = tweepy.OAuth1UserHandler(
 # API 객체 생성
 api = tweepy.API(auth)
 
+# DB 연결
+conn = psycopg2.connect(
+    host=host, port=port, database=database, user=user, password=password
+)
+cur = conn.cursor()
+
 # 크롤링할 키워드
 keywords = ["보안", "해킹", "유출", "개인정보", "해킹"]
 
-# 최신 트윗 100건 크롤링
+# 최신 트윗 100건 크롤링 및 DB 저장
 for keyword in keywords:
     print(f"Keyword: {keyword}")
     tweets = api.search_tweets(q=keyword, count=100, tweet_mode="extended")
     for tweet in tweets:
-        print(
-            f"{tweet.user.name}, {tweet.user.screen_name}, "
-            f"{tweet.created_at}, {tweet.full_text}"
+        author_name = tweet.user.name
+        author_screen_name = tweet.user.screen_name
+        created_at = tweet.created_at
+        full_text = tweet.full_text
+
+        # SQL 문 작성
+        insert_query = sql.SQL(
+            "INSERT INTO {} ({}, {}, {}, {}) VALUES (%s, %s, %s, %s)"
+        ).format(
+            sql.Identifier("tweets"),
+            sql.Identifier("author_name"),
+            sql.Identifier("author_screen_name"),
+            sql.Identifier("created_at"),
+            sql.Identifier("full_text"),
         )
+
+        # 크롤링 결과 DB 저장
+        cur.execute(
+            insert_query,
+            (author_name, author_screen_name, created_at, full_text),
+        )
+        conn.commit()
