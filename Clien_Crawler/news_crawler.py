12ea734640769b60af79bdaeb2aa4df729de2072
Heewon Yu <narsis.yu@gmail.com>
2023-04-08 22:12:18 +0900

    Clien New 크롤링 최초 버전

--- Clien_Crawler/news_crawler.py
+++ Clien_Crawler/news_crawler.py
@@ -3,17 +3,12 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.clien.net/service/board/news'
 
-# Send an HTTP request to the URL
 response = requests.get(url)
 content = response.content
 
-# Parse the HTML content
 soup = BeautifulSoup(content, 'html.parser')
-
-# Extract the article list
 article_list = soup.find('div', {'class': 'list_content'})
 
-# Extract article information
 for article in article_list.find_all('div', {'class': 'list_item'}):
     title = article.find('span', {'class': 'subject_fixed'}).text.strip()
     link = 'https://www.clien.net' + article.find('a', {'class': 'list_subject'}).get('href')
@@ -23,7 +18,13 @@ for article in article_list.find_all('div', {'class': 'list_item'}):
     article_soup = BeautifulSoup(article_content, 'html.parser')
     
     content = article_soup.find('div', {'class': 'post_content'}).text.strip()
-    date = article_soup.find('span', {'class': 'post_author'}).find_next('span', {'class': 'timestamp'}).text.strip()
+    
+    post_author = article_soup.find('span', {'class': 'post_author'})
+    if post_author:
+        date = post_author.find_next('span', {'class': 'timestamp'}).text.strip() if post_author.find_next('span', {'class': 'timestamp'}) else 'N/A'
+    else:
+        date = 'N/A'
+    
     author = article_soup.find('span', {'class': 'nickname'}).text.strip()
 
     print('제목:', title)
