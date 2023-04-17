7504739a15c4c16139dddac78bfb8ef66366d59a
Heewon Yu <narsis.yu@gmail.com>
2023-04-18 06:21:56 +0900

    20230418_아침 최종

--- WebAPP/app.py
+++ WebAPP/app.py
@@ -6,6 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:dlQmsdl00@localho
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(5), nullable=False)
@@ -17,15 +18,19 @@ class Member(db.Model):
     def __repr__(self):
         return f"<Member {self.name}>"
 
+
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+
 @app.route('/')
 def index():
     search = request.args.get('search', '')
-    members = Member.query.filter(Member.name.contains(search) | Member.phone.contains(search) | Member.company.contains(search)).all()
+    members = Member.query.filter(
+        Member.name.contains(search) | Member.phone.contains(search) | Member.company.contains(search)).all()
     return render_template('index.html', members=members)
+
 
 @app.route('/new', methods=['GET', 'POST'])
 def new_member():
@@ -41,10 +46,12 @@ def new_member():
         return redirect(url_for('index'))
     return render_template('new.html')
 
+
 @app.route('/detail/<int:member_id>')
 def detail(member_id):
     member = Member.query.get(member_id)
     return render_template('detail.html', member=member)
+
 
 @app.route('/edit/<int:member_id>', methods=['GET', 'POST'])
 def edit_member(member_id):
@@ -59,12 +66,14 @@ def edit_member(member_id):
         return redirect(url_for('detail', member_id=member.id))
     return render_template('edit.html', member=member)
 
+
 @app.route('/delete/<int:member_id>', methods=['POST'])
 def delete_member(member_id):
     member = Member.query.get(member_id)
     db.session.delete(member)
     db.session.commit()
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
