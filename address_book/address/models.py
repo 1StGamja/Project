f132c3acd42234f2e01e6b224408c6441791e2bc
Heewon Yu <narsis.yu@gmail.com>
2023-04-21 20:07:45 +0900

    20230421_임시

---
+++ address_book/address/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Address(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    company_name = models.CharField(max_length=100)
+    relationship = models.CharField(max_length=50)
+    registered_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
